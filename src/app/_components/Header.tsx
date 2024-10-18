af861d67d7a7279327453cda6769abc6d9cfef27
Samir Sadek <samirsadek2009@gmail.com>
2024-10-18 21:39:19 +0600

    feature navbar

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { ChevronDown, Heart } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-between items-center px-20 py-5 font-medium">
+      <div>
+        <Image src="/logo.png" alt="logo" width={90} height={44} />
+      </div>
+      <div className="flex gap-8 items-center text-[15px]">
+        <h2>Home</h2>
+        <h2 className="flex gap-1 items-center">Categories <ChevronDown className="w-5 h-5" /></h2>
+        <h2>Blogs</h2>
+        <h2 className="flex gap-1 items-center">English <ChevronDown className="w-5 h-5" /></h2>
+        <h2 className="flex gap-1 items-center">USD(US$) <ChevronDown className="w-5 h-5" /></h2>
+        <Image src='/shopping-cart.png' width={20} height={20}/>
+        <h2 ><Heart className="w-5 h-5" /></h2>
+        <h2 className="flex gap-1 items-center">Log In <ChevronDown className="w-5 h-5" /></h2>
+        <Button className="px-5 py-2">Sign Up</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
