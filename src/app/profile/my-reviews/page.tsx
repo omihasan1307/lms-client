f0b426074125926726b8c8af99adca2b9767090e
Samir Sadek <samirsadek2009@gmail.com>
2024-11-01 12:50:35 +0600

    page ui design profile/my-reviews

---
+++ src/app/profile/my-reviews/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { TfiEraser } from 'react-icons/tfi'
+import ReviewCard from "../../_components/cards/ReviewCard"
+import ReviewedCard from "../../_components/cards/ReviewedCard"
+
+function MyReviews() {
+  return (
+    <div>
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-bold ">
+           My Reviews
+          </h1>
+          <button className="text-gray-500 hover:text-gray-700 flex justify-center items-center gap-2 text-[14px]">
+          <TfiEraser />
+          Clear All
+          </button>
+        </div>
+        <ReviewCard />
+        <ReviewedCard/>
+    </div>
+  )
+}
+
+export default MyReviews
